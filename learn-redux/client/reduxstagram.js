6b6bd05680b36db5a0d667b67463af0a4710dacd
Devon Norris <norris.devon@gmail.com>
2018-06-22 14:35:15 -0400

    put some router action

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,5 +7,21 @@ import css from './styles/style.styl';
 
 //import components
 import Main from './components/Main';
+import single from './components/single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postid" component={single}></Route>
+    </Route>
+  </Router>
+)
+
+render(
+  router, document.getElementById('root')
+);
